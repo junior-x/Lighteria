a33c7f5685fd49c78ce4a2fb9cfa58464613514d
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-15 20:55:56 -0300

    Delete src directory

--- src/assets/index.js
+++
@@ -1,6 +0,0 @@
-// Images
-import iconBag from './images/icone-sacola.png';
-import iconCube from './images/icone-cubo.png';
-import iconMenu from './images/icone-menu.png';
-
-export { iconBag, iconCube, iconMenu };
--- src/views/ListaProdutos/componentes/ListaProdutos/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from 'react-native';
-import Cabecalho from '../Cabecalho';
-
-const ListaProdutos = () => {
-  return (
-    <Lista />
-  );
-};
