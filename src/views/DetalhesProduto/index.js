9ec4aa3605ca4ac94603acaacac2c1dc1d21915d
junior <72835263+vampyrsoda@users.noreply.github.com>
2021-04-15 20:56:40 -0300

    Add files via upload

---
+++ src/views/DetalhesProduto/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+const DetalhesProduto = () => {
+    return(
+        <View>
+            <Text>
+                DetalhesProduto
+            </Text>
+        </View>
+    );
+}
+
+export default DetalhesProduto;
